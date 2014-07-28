f247310b29ba795b4578877f0253e8108d28ecca
Will Adams <wawadams@gmail.com>
2014-07-28 16:34:00 -0400

    Update default.rb

--- cookbooks/node_0_10/recipes/default.rb
+++ cookbooks/node_0_10/recipes/default.rb
@@ -8,4 +8,16 @@ if app_server?
       backup false
       mode 0644
     end
+    enable_package "net-libs/nodejs" do
+    version "0.10.28"
+end
+ 
+package "net-libs/nodejs" do
+  version "0.10.28"
+  action :install
+end
+ 
+eselect "0.10.28" do
+  slot 'nodejs'
+
 end
