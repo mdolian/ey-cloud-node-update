d354a26860c94739e349d7587cbc2a89b7c99f65
Will Adams <wawadams@gmail.com>
2014-07-28 16:35:24 -0400

    Update default.rb

--- cookbooks/node_0_10/recipes/default.rb
+++ cookbooks/node_0_10/recipes/default.rb
@@ -1,13 +1,5 @@
-if app_server?
+if app_server? or util?
 
-    # create new nodejs_available_versions_json
-    template "/opt/nodejs/nodejs_available_versions.json" do
-      source 'nodejs_available_versions.json'
-      owner node[:owner_name]
-      group node[:owner_name]
-      backup false
-      mode 0644
-    end
     enable_package "net-libs/nodejs" do
     version "0.10.28"
 end
